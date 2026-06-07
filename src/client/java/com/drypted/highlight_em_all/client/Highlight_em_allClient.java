ac39c3f690ddfacb420ef332ecdaf28b5d100500
PhantomZ3 <sd.phantomz3@gmail.com>
2026-06-07 13:55:23 +0500

    initial commit

---
+++ src/client/java/com/drypted/highlight_em_all/client/Highlight_em_allClient.java
@@ -0,0 +1,10 @@
+package com.drypted.highlight_em_all.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Highlight_em_allClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
